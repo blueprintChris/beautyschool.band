57a8985bfae345dea751d3343bc5e0fcde4fdf80
Chris Moran <cmoran1989@hotmail.co.uk>
2021-06-17 18:38:23 +0100

    feat: styling

--- src/components/Hero/styles.js
+++ src/components/Hero/styles.js
@@ -16,7 +16,8 @@ export const Logo = styled.img`
   border: solid;
   border-width: 6px;
   border-image-slice: 1;
-  border-image-source: linear-gradient(180deg, rgba(127, 83, 31, 1) 0%, rgba(201, 143, 33, 1) 100%);
+  border-image-source: ${(props) =>
+    `linear-gradient(180deg, ${props.theme.colours.darkGold} 0%, ${props.theme.colours.lightGold} 100%)`};
   padding: 2rem;
   background-color: rgba(0, 0, 0, 0.2);
 `;
