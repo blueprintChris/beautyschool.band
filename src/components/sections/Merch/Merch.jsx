a2542cd1e136bfd54b2eaee72f31a6a3192ed00f
Chris Moran <cmoran1989@hotmail.co.uk>
2021-12-01 14:02:39 +0000

    feat: added image api call for merch

--- src/components/sections/Merch/Merch.jsx
+++ src/components/sections/Merch/Merch.jsx
@@ -1,15 +1,22 @@
 import React, { useContext } from 'react';
 import NavContext from '../../../context/NavContext';
-import { ContentItem } from '../../common';
+import useApi from '../../../hooks/useApi';
+import { ContentItem, Spinner } from '../../common';
 import { Link } from './styles';
 
 const Merch = () => {
   const { refs } = useContext(NavContext);
+  const { response, error, isLoading } = useApi('images/merch');
+
+  if (isLoading) {
+    return <Spinner text='Fetching merch...' />;
+  }
 
   return (
     <ContentItem header='merch' ref={refs.merch}>
       <Link href='https://beautyschooluk.bandcamp.com/merch' target='_blank'>
-        <img src='images/merch-web-450.png' alt='Get Merch' />
+        {error && <img src='images/merch-web-450.png' alt='Get Merch' />}
+        {response && <img src={`https://beauty-school-content.s3.eu-west-1.amazonaws.com/${response[0].Key}`} />}
       </Link>
     </ContentItem>
   );
