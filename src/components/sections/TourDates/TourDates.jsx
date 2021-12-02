f7891c7aa02b77013cc58b93f43c747ec58a50c9
Chris Moran <cmoran1989@hotmail.co.uk>
2021-12-02 11:14:29 +0000

    feat: updated nav and fixed refs, updated styling for nav bar

--- src/components/sections/TourDates/TourDates.jsx
+++ src/components/sections/TourDates/TourDates.jsx
@@ -1,4 +1,4 @@
-import React, { useContext, useState } from 'react';
+import React, { useContext } from 'react';
 import NavContext from '../../../context/NavContext';
 import { ContentItem } from '../../common';
 import { Event, EventInfo, EventDate, EventVenue, EventButton } from './styles';
