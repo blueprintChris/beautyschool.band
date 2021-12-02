771da076071c3347c1dfce49c5efe09f37c7cb0e
Chris Moran <cmoran1989@hotmail.co.uk>
2021-12-02 11:47:27 +0000

    feat: added youtube height sizes for mobile

--- src/components/sections/Watch/Watch.jsx
+++ src/components/sections/Watch/Watch.jsx
@@ -1,17 +1,19 @@
 import React, { useContext } from 'react';
+import MobileContext from '../../../context/MobileContext';
 import NavContext from '../../../context/NavContext';
 import { ContentItem } from '../../common';
 import { Wrapper } from './styles';
 
 const Watch = props => {
   const { refs } = useContext(NavContext);
+  const { isMobile } = useContext(MobileContext);
 
   return (
     <ContentItem header='watch' ref={refs.watch}>
       <Wrapper>
         <iframe
           width='100%'
-          height='600'
+          height={isMobile ? '240' : '560'}
           src={`https://www.youtube.com/embed/wJElPbiUMIk`}
           title='YouTube video player'
           frameBorder='0'
