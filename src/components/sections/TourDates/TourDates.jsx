dbc9a5c2215ada482183e4ed827495ca4836f0aa
Chris Moran <cmoran1989@hotmail.co.uk>
2022-02-01 20:01:15 +0000

    feat: added tour dates for trash boat. updated event components to loop

--- src/components/sections/TourDates/TourDates.jsx
+++ src/components/sections/TourDates/TourDates.jsx
@@ -3,6 +3,34 @@ import NavContext from '../../../context/NavContext';
 import { ContentItem } from '../../common';
 import { Event, EventInfo, EventDate, EventVenue, EventButton } from './styles';
 
+const events = [
+  {
+    date: 'Mon 14 Mar, 2022 (w/ Trash Boat)',
+    venue: 'Polar Bear Music Club, Hull',
+    ticketLink: 'https://www.seetickets.com/event/trash-boat/polar-bear-music-club/2156669',
+  },
+  {
+    date: 'Tue 15 Mar 2022 (w/ Trash Boat)',
+    venue: 'The Crescent, York',
+    ticketLink: 'https://www.seetickets.com/event/trash-boat/the-crescent/2156662',
+  },
+  {
+    date: 'Wed 16 Mar 2022 (w/ Trash Boat)',
+    venue: 'Corporation, Sheffield',
+    ticketLink: 'https://www.corporation.org.uk/gig/2549',
+  },
+  {
+    date: 'Sat 9th April, 2022',
+    venue: 'The Key Club, Leeds',
+    ticketLink: 'https://slamdunkmusic.seetickets.com/event/beauty-school/the-key-club/2161961',
+  },
+  {
+    date: 'Fri 3rd June / Sat 4th June, 2022',
+    venue: 'Slam Dunk Festival, 2022',
+    ticketLink: 'https://slamdunk.seetickets.com/tour/slam-dunk-festival-2022?pre=pastbooker',
+  },
+];
+
 const TourDates = () => {
   const { refs } = useContext(NavContext);
   // const [tourDates, setTourDates] = useState([]);
@@ -21,27 +49,17 @@ const TourDates = () => {
   return (
     // <ContentItem header='tour' ref={refs.listen} img='images/songkick.png' imgAlt='Powered by Songkick'>
     <ContentItem header='tickets' ref={refs.tickets}>
-      <Event key='keyclub'>
-        <EventInfo>
-          <EventDate>Sat 9th April, 2022</EventDate>
-          <EventVenue>The Key Club, Leeds</EventVenue>
-        </EventInfo>
-        <EventButton
-          href='https://slamdunkmusic.seetickets.com/event/beauty-school/the-key-club/2161961'
-          target='_blank'
-        >
-          Tickets
-        </EventButton>
-      </Event>
-      <Event key='slamdunk'>
-        <EventInfo>
-          <EventDate>Fri 3rd June / Sat 4th June, 2022</EventDate>
-          <EventVenue>Slam Dunk Festival 2022</EventVenue>
-        </EventInfo>
-        <EventButton href='https://slamdunk.seetickets.com/tour/slam-dunk-festival-2022?pre=pastbooker' target='_blank'>
-          Tickets
-        </EventButton>
-      </Event>
+      {events.map((event, index) => (
+        <Event key={index}>
+          <EventInfo>
+            <EventDate>{event.date}</EventDate>
+            <EventVenue>{event.venue}</EventVenue>
+          </EventInfo>
+          <EventButton href={event.ticketLink} target='_blank'>
+            Tickets
+          </EventButton>
+        </Event>
+      ))}
 
       {/* {tourDates.length > 1 ? (
         tourDates.map(event => (
