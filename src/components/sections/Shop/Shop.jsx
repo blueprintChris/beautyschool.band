c4884947c525631d2485ef97ebb59fbc4bef6049
Chris Moran <cmoran1989@hotmail.co.uk>
2022-03-31 10:01:44 +0100

    feat: updated store link

--- src/components/sections/Shop/Shop.jsx
+++ src/components/sections/Shop/Shop.jsx
@@ -14,11 +14,9 @@ const Shop = () => {
 
   return (
     <ContentItem header='shop' ref={refs.shop}>
-      <Link href='https://beautyschooluk.bandcamp.com/merch' target='_blank'>
+      <Link href='https://store.beautyschool.band' target='_blank'>
         {error && <img src='images/merch-web-450.png' alt='Get Merch' />}
-        {response && (
-          <img src={`https://beauty-school-content.s3.eu-west-1.amazonaws.com/${response[0].Key}`} alt='Get Merch' />
-        )}
+        {response && <img src={`https://beauty-school-content.s3.eu-west-1.amazonaws.com/${response[0].Key}`} alt='Get Merch' />}
       </Link>
     </ContentItem>
   );
