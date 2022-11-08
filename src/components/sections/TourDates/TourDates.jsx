073e19a2523498425f841f76fe936bd9b04ff012
Chris Moran <cmoran1989@hotmail.co.uk>
2022-11-08 13:03:53 +0000

    feat: temporary wonder years dates added

--- src/components/sections/TourDates/TourDates.jsx
+++ src/components/sections/TourDates/TourDates.jsx
@@ -7,6 +7,100 @@ import { Event, EventInfo, EventDate, EventVenue, EventButton, StyledText } from
 const songkickKey = process.env.REACT_APP_SONGKICK_KEY;
 const songkickUrl = process.env.REACT_APP_SONGKICK_URL;
 
+const wonderYearsTourDates = [
+  {
+    id: 0,
+    start: {
+      date: '2022-11-08T19:00Z',
+    },
+    venue: {
+      displayName: 'Electric Ballroom',
+    },
+    location: {
+      city: 'London',
+    },
+    uri: 'https://www.seetickets.com/event/the-wonder-years/electric-ballroom/2386235',
+  },
+  {
+    id: 1,
+    start: {
+      date: '2022-11-09T19:00Z',
+    },
+    venue: {
+      displayName: 'O2 Academy',
+    },
+    location: {
+      city: 'Birmingham',
+    },
+    uri: 'https://www.seetickets.com/event/the-wonder-years/o2-academy2-birmingham/2386236',
+  },
+  {
+    id: 1,
+    start: {
+      date: '2022-11-10T19:00Z',
+    },
+    venue: {
+      displayName: 'Engine Rooms',
+    },
+    location: {
+      city: 'Southampton',
+    },
+    uri: 'https://www.seetickets.com/event/the-wonder-years/engine-rooms/2389874',
+  },
+  {
+    id: 1,
+    start: {
+      date: '2022-11-12T19:00Z',
+    },
+    venue: {
+      displayName: 'SWX',
+    },
+    location: {
+      city: 'Bristol',
+    },
+    uri: 'https://www.seetickets.com/event/the-wonder-years/swx/2386595',
+  },
+  {
+    id: 1,
+    start: {
+      date: '2022-11-13T19:00Z',
+    },
+    venue: {
+      displayName: 'Manchester Academy 2',
+    },
+    location: {
+      city: 'Manchester',
+    },
+    uri: 'https://www.seetickets.com/event/the-wonder-years/manchester-academy-2/2386239',
+  },
+  {
+    id: 1,
+    start: {
+      date: '2022-11-14T19:00Z',
+    },
+    venue: {
+      displayName: "Saint Luke's",
+    },
+    location: {
+      city: 'Glasgow',
+    },
+    uri: 'https://www.seetickets.com/event/the-wonder-years/saint-luke-s/2389155',
+  },
+  {
+    id: 1,
+    start: {
+      date: '2022-11-15T19:00Z',
+    },
+    venue: {
+      displayName: 'Leeds University Stylus',
+    },
+    location: {
+      city: 'Leeds',
+    },
+    uri: 'https://www.seetickets.com/event/the-wonder-years/leeds-university-stylus/2386602',
+  },
+];
+
 const TourDates = () => {
   const { refs } = useContext(NavContext);
   const [tourDates, setTourDates] = useState([]);
@@ -26,8 +120,8 @@ const TourDates = () => {
 
   return (
     <ContentItem header='tour' ref={refs.tickets} img='images/songkick.png' imgAlt='Powered by Songkick'>
-      {tourDates.length > 1 ? (
-        tourDates.map(event => (
+      {wonderYearsTourDates.length > 0 ? (
+        wonderYearsTourDates.map(event => (
           <Event key={event.id}>
             <EventInfo>
               <EventDate>{format(new Date(event.start.date), 'EEE do MMM, yyyy')}</EventDate>
