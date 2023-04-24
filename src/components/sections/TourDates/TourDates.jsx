36167e665415d0818f212aacf75ecf13ed085d27
Chris Moran <cmoran1989@hotmail.co.uk>
2023-04-24 19:12:55 +0100

    fix: added missing tour dates

--- src/components/sections/TourDates/TourDates.jsx
+++ src/components/sections/TourDates/TourDates.jsx
@@ -7,6 +7,100 @@ import { Event, EventInfo, EventDate, EventVenue, EventButton, StyledText } from
 const songkickKey = process.env.REACT_APP_SONGKICK_KEY;
 const songkickUrl = process.env.REACT_APP_SONGKICK_URL;
 
+const newDates = [
+  {
+    id: 0,
+    start: {
+      date: '2023-04-29',
+    },
+    venue: {
+      displayName: 'Teddy Rocks Festival',
+    },
+    location: {
+      city: 'Dorset',
+    },
+    uri: 'https://teddyrocks.co.uk/tickets',
+  },
+  {
+    id: 1,
+    start: {
+      date: '2023-05-30',
+    },
+    venue: {
+      displayName: 'Essigrabrik',
+    },
+    location: {
+      city: 'Cologne',
+    },
+    uri: 'https://www.eventim.de/event/less-than-jake-essigfabrik-16761224/',
+  },
+  {
+    id: 2,
+    start: {
+      date: '2023-05-31',
+    },
+    venue: {
+      displayName: 'Backstage Werk',
+    },
+    location: {
+      city: 'Munich',
+    },
+    uri: 'https://www.eventim.de/event/less-than-jake-backstage-muenchen-16753516/',
+  },
+  {
+    id: 3,
+    start: {
+      date: '2023-06-03',
+    },
+    venue: {
+      displayName: 'Slam Dunk Festival Italy',
+    },
+    location: {
+      city: 'Rimini',
+    },
+    uri: 'https://www.slamdunkfestival.com/italy-tickets',
+  },
+  {
+    id: 4,
+    start: {
+      date: '2023-06-09',
+    },
+    venue: {
+      displayName: '02 Academy',
+    },
+    location: {
+      city: 'Islington',
+    },
+    uri: 'https://www.songkick.com/concerts/41077549-state-champs-at-o2-academy-islington',
+  },
+  {
+    id: 5,
+    start: {
+      date: '2023-06-11',
+    },
+    venue: {
+      displayName: 'Download Festival',
+    },
+    location: {
+      city: 'Donnington',
+    },
+    uri: 'https://downloadfestival.co.uk/tickets/',
+  },
+  {
+    id: 6,
+    start: {
+      date: '2023-07-07',
+    },
+    venue: {
+      displayName: '2000 Trees Festival',
+    },
+    location: {
+      city: 'Cheltenham',
+    },
+    uri: 'https://2000trees.co.uk/tickets',
+  },
+];
+
 const TourDates = () => {
   const { refs } = useContext(NavContext);
   const [tourDates, setTourDates] = useState([]);
@@ -17,6 +111,12 @@ const TourDates = () => {
         const response = await fetch(`${songkickUrl}${songkickKey}`);
         const data = await response.json();
         const upcomingShows = data.resultsPage.results.event.filter(event => new Date(event.start.date) > new Date());
+        upcomingShows.push(...newDates);
+
+        upcomingShows.sort((a, b) => {
+          return new Date(a.start.date) - new Date(b.start.date);
+        });
+
         setTourDates(upcomingShows);
       } catch (err) {
         console.error(err);
