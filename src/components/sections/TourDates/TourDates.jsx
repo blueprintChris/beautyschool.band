a999811b9badceeeb4685a64e05683faba15faa9
Chris Moran <cmoran1989@hotmail.co.uk>
2023-11-08 12:30:02 +0000

    feat: removed old tour dates

--- src/components/sections/TourDates/TourDates.jsx
+++ src/components/sections/TourDates/TourDates.jsx
@@ -7,116 +7,22 @@ import { Event, EventInfo, EventDate, EventVenue, EventButton, StyledText, Event
 const songkickKey = process.env.REACT_APP_SONGKICK_KEY;
 const songkickUrl = process.env.REACT_APP_SONGKICK_URL;
 
-const newDates = [
-  {
-    id: 0,
-    start: {
-      date: '2023-04-29',
-    },
-    venue: {
-      displayName: 'Teddy Rocks Festival',
-    },
-    location: {
-      city: 'Dorset',
-    },
-    uri: 'https://teddyrocks.co.uk/tickets',
-  },
-  {
-    id: 1,
-    start: {
-      date: '2023-05-30',
-    },
-    performance: [
-      { displayName: 'Less Than Jake' },
-      {
-        displayName: 'Bowling For Soup',
-      },
-    ],
-    venue: {
-      displayName: 'Essigfabrik',
-    },
-    location: {
-      city: 'Cologne',
-    },
-    uri: 'https://www.eventim.de/event/less-than-jake-essigfabrik-16761224/',
-  },
-  {
-    id: 2,
-    start: {
-      date: '2023-05-31',
-    },
-    performance: [
-      { displayName: 'Less Than Jake' },
-      {
-        displayName: 'Bowling For Soup',
-      },
-    ],
-    venue: {
-      displayName: 'Backstage Werk',
-    },
-    location: {
-      city: 'Munich',
-    },
-    uri: 'https://www.eventim.de/event/less-than-jake-backstage-muenchen-16753516/',
-  },
-  {
-    id: 3,
-    start: {
-      date: '2023-06-03',
-    },
-    venue: {
-      displayName: 'Slam Dunk Festival Italy',
-    },
-    location: {
-      city: 'Rimini',
-    },
-    uri: 'https://www.slamdunkfestival.com/italy-tickets',
-  },
-  {
-    id: 4,
-    start: {
-      date: '2023-06-09',
-    },
-    venue: {
-      displayName: '02 Academy',
-    },
-    location: {
-      city: 'Islington',
-    },
-    performance: [
-      {
-        displayName: 'State Champs',
-      },
-    ],
-    uri: 'https://www.songkick.com/concerts/41077549-state-champs-at-o2-academy-islington',
-  },
-  {
-    id: 5,
-    start: {
-      date: '2023-06-11',
-    },
-    venue: {
-      displayName: 'Download Festival',
-    },
-    location: {
-      city: 'Donnington',
-    },
-    uri: 'https://downloadfestival.co.uk/tickets/',
-  },
-  {
-    id: 6,
-    start: {
-      date: '2023-08-11',
-    },
-    venue: {
-      displayName: 'The Key Club',
-    },
-    location: {
-      city: 'Leeds',
-    },
-    uri: 'https://www.seetickets.com/event/beauty-school/the-key-club/2672076',
-  },
-];
+/** Example tour date array */
+// const newDates = [
+//   {
+//     id: 0,
+//     start: {
+//       date: '2023-04-29',
+//     },
+//     venue: {
+//       displayName: 'Teddy Rocks Festival',
+//     },
+//     location: {
+//       city: 'Dorset',
+//     },
+//     uri: 'https://teddyrocks.co.uk/tickets',
+//   },
+// ];
 
 const TourDates = () => {
   const { refs } = useContext(NavContext);
@@ -128,7 +34,6 @@ const TourDates = () => {
         const response = await fetch(`${songkickUrl}${songkickKey}`);
         const data = await response.json();
         const upcomingShows = data.resultsPage.results.event.filter(event => new Date(event.start.date) > new Date());
-        upcomingShows.push(...newDates);
 
         upcomingShows.sort((a, b) => {
           return new Date(a.start.date) - new Date(b.start.date);
